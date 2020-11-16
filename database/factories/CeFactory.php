f4634104a91c334bc5142482feb328f2af105893
Ireniushka <irenerengelruiz@gmail.com>
2020-11-16 13:17:57 +0100

    subida del proyecto

---
+++ database/factories/CeFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Ce::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/Task_doneFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Task_done::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
