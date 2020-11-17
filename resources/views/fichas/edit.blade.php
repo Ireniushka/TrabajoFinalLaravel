b06d3ca02ff563e3f5f4e080b0bab3a39f398aed
alvaromoli13 <soulalvaro@gmail.com>
2020-11-17 18:51:46 +0100

    boton editar1
    1

--- resources/views/fichas/edit.blade.php
+++ resources/views/fichas/edit.blade.php
@@ -1,8 +1,12 @@
-<form action="" method="post">
+<form action="{{url('/fichas/'.$fichaAlumno->id)}}" method="post" enctype="multipart/form-data">
+{{csrf_field()}}
+{{ method_field('PATCH')}}
     <label for="description">{{'Descripción'}}</label>
     <input type="text" name="description" id="description" value="{{ $ficha->description}}">
     </br>
 
     <label for="date">{{'Fecha'}}</label>
     <input type="date" name="date" id="date" value="{{ $ficha->date}}">  
+
+    <input type="submit" value="Editar">
 </form>
